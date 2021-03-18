4a18e452a2633a702a1734c02b02e7582aa6894a
Racer <reaganjoseph26@yahoo.com>
2021-03-18 18:41:07 -0400

    contact form created

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,9 +1,70 @@
-import React from 'react';
-
+import React, {useState} from 'react';
+import { validateEmail } from '../../utils/helpers';
 function Contact(props) {
+
+    const [formState, setFormState] = useState({ name: '', email: '', message: '' });
+    const { name, email, message } = formState;
+    const [errorMessage, setErrorMessage] = useState('');
+
+    //sync the state of the component formState with the user inputs.
+    function handleChange(e) {
+        if (e.target.name === 'email') {
+            const isValid = validateEmail(e.target.value);
+            console.log(isValid);
+            // isValid conditional statement
+            if (!isValid) {
+                setErrorMessage('Your email is invalid.');
+            } else {
+                if (!e.target.value.length) {
+                    setErrorMessage(`${e.target.name} is required.`);
+                  } else {
+                    setErrorMessage('');
+                  }
+    
+            }
+          }  
+        // Without the spread operator(...), the formState object would be overwritten to only contain the name: value key pair.
+        if (!errorMessage) {
+            setFormState({ ...formState, [e.target.name]: e.target.value });
+        }
+    }
+
+    function handleSubmit(e) {
+        e.preventDefault();
+        console.log(formState);
+    }
+
     return (
         <div>
-            { props.renderedComponent === "Contact" ? (<>Contact is selected</>) : (<>Contact not selected</>) }
+            { props.renderedComponent === "Contact" ? (<>
+                <section>
+          <h1 data-testid="h1tag">Contact me</h1>
+          <form id="contact-form" onSubmit={handleSubmit}>
+           <div>
+                <label htmlFor="name">Name:</label>
+                <input type="text" defaultValue={name} onBlur={handleChange} name="name" />
+            </div>
+            <div>
+                <label htmlFor="email">Email address:</label>
+                <input type="email" defaultValue={email} name="email" onBlur={handleChange} />
+            </div>
+            <div>
+                <label htmlFor="message">Message:</label>
+                <textarea name="message" defaultValue={message} onBlur={handleChange} rows="5" />
+            </div>
+
+            {errorMessage && (
+                <div>
+                    <p className="error-text">{errorMessage}</p>
+                </div>
+            )}
+
+            <button data-testid="button" type="submit">Submit</button>
+
+          </form>
+        </section>
+            
+            </>) : (<>Contact not selected</>) }
         </div>
     );
 }
