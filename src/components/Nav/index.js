2560708ec14014e63d4789cadfd6eb24a43a3b80
Racer <reaganjoseph26@yahoo.com>
2021-03-18 21:52:28 -0400

    projetcs content placed

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -3,22 +3,22 @@ import React from 'react';
 function Nav(props) {
     return (
         <>
-            <nav class="navbar navbar-expand-lg navbar-light">
-                <div class="container-fluid">
-                    <a class="navbar-brand" href="#">The Portfolio </a>
-                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-                        <span class="navbar-toggler-icon"></span>
+            <nav className="navbar navbar-expand-lg navbar-light">
+                <div className="container-fluid">
+                    <a className="navbar-brand" href="#">The Portfolio </a>
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
                     </button>
-                    <div class="collapse navbar-collapse" id="navbarNavDropdown">
-                        <ul class="navbar-nav">
-                            <li class="nav-item bg-dark" onClick={() => {props.setRenderedComponent("About")}}>
-                                <a class="nav-link active" aria-current="page" href="#">About Me</a>
+                    <div className="collapse navbar-collapse" id="navbarNavDropdown">
+                        <ul className="navbar-nav">
+                            <li className="nav-item bg-dark" onClick={() => {props.setRenderedComponent("About")}}>
+                                <a className="nav-link active" aria-current="page" href="#">About Me</a>
                             </li>
-                            <li class="nav-item" onClick={() => {props.setRenderedComponent("Projects")}}>
-                                <a class="nav-link active" aria-current="page" href="#">Projects</a>
+                            <li className="nav-item" onClick={() => {props.setRenderedComponent("Projects")}}>
+                                <a className="nav-link active" aria-current="page" href="#">Projects</a>
                             </li>
-                            <li class="nav-item" onClick={() => {props.setRenderedComponent("Contact")}}>
-                                <a class="nav-link active" aria-current="page" href="#">Contact Me</a>
+                            <li className="nav-item" onClick={() => {props.setRenderedComponent("Contact")}}>
+                                <a className="nav-link active" aria-current="page" href="#">Contact Me</a>
                             </li>
                         </ul>
                     </div>
--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -1,10 +1,45 @@
 import React from 'react';
 
-function Projects(props) {
+
+function Projects() {
     return (
-        <div>
-             { props.renderedComponent === "Projects" ? (<>Projects is selected</>) : (<>Projects not selected</>) }
-        </div>
+            <div id="projects" className="row align-items-start">
+                <div className="col-12">
+                    <h2 id="projects">Projects</h2>
+                </div>
+                <div className="col-6">
+                  <h3>Run Buddy</h3>
+                  <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ducimus esse aspernatur animi, veritatis saepe deleniti aliquid a, quidem officiis quod unde laboriosam iste aperiam ratione illo. Quos voluptate autem aperiam!</p>
+                </div>
+                <div className="col-6">
+                    <a href="https://reaganjoseph26.github.io/run-buddy/"><img src="../../assets/images/runbuddy.jpg" alt="RUN BUDDY"/></a>
+                </div>
+
+                <div className="col-6">
+                  <h3>Movie Hunt</h3>
+                  <p>Movie Hunt is a web application dedicated to movie lovers. Users are able to search the site for any movie they wish as well as save the movie. The Movie Database API provides the data for the site. For more informoation on the technologices used, the process it ook to create the application, and my collaborators, visist the repositiory here:  <a href="https://github.com/reaganjoseph26/Movie-Hunt.git">Movie Hunt Repo.</a></p>
+                </div>
+                <div className="col-6">      
+                  <a href="https://reaganjoseph26.github.io/Movie-Hunt/"><img src="./images/movie-hunt.jpg" alt="Movie Hunt"/></a>
+                </div>
+
+                <div className="col-6">
+                  <h3>Taskmaster Pro</h3>
+                  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt quo, ad voluptates, voluptatum cumque praesentium eum culpa vitae facere, quis ut harum? Deserunt voluptatem omnis repudiandae aliquam tenetur ex nam.</p>
+                </div>
+                <div className="col-6">
+                  <a href="https://reaganjoseph26.github.io/taskmaster-pro/"><img src="./images/taskmaster-pro.jpg" alt="Taskmaster-pro"/></a>
+                </div>
+
+                <div className="col-6">
+                  <h3>Work-Day Scheduler</h3>
+                  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam, iste atque. Earum aliquid recusandae perferendis numquam quod autem aut rem consectetur nulla possimus? Consequatur soluta, voluptates quisquam beatae placeat illo?</p>
+                </div>
+                <div className="col-6"> 
+                <a href="https://reaganjoseph26.github.io/Work-Day-Scheduler-/"><img src="./images/work-day-sched.jpg" alt="Work-Day Scheduler"/></a>
+                </div>
+            </div>
+        
     );
 }
 
