9bfcdef3b28e2a066dff9ab5b60dda977d0cce9b
Racer <reaganjoseph26@yahoo.com>
2021-05-21 17:38:56 -0400

    contact info change

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -59,33 +59,6 @@ function Contact() {
             
         </section>
 
-
-                /* <section>
-          <h1 data-testid="h1tag">Contact me</h1>
-          <form id="contact-form" onSubmit={handleSubmit}>
-           <div>
-                <label htmlFor="name">Name:</label>
-                <input type="text" defaultValue={name} onBlur={handleChange} name="name" />
-            </div>
-            <div>
-                <label htmlFor="email">Email address:</label>
-                <input type="email" defaultValue={email} name="email" onBlur={handleChange} />
-            </div>
-            <div>
-                <label htmlFor="message">Message:</label>
-                <textarea name="message" defaultValue={message} onBlur={handleChange} rows="5" />
-            </div>
-
-            {errorMessage && (
-                <div>
-                    <p className="error-text">{errorMessage}</p>
-                </div>
-            )}
-
-            <button data-testid="button" type="submit">Submit</button>
-
-          </form>
-        </section> */
             
     );
 }
